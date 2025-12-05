10b42da239525d2b0e47f805ab33521b6bfbc5c4
Odyssey-M <quweiqi952@gmail.com>
2025-12-05 18:28:00 +0000

    puzzle detect

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -4,126 +4,111 @@ using UnityEngine;
 
 public class GamePlay : MonoBehaviour
 {
-    // 小场景摄像机
+
     [SerializeField] private Transform[] littleSceneCams;
-    // 小场景中心（相机围绕次中心旋转）
+
     [SerializeField] private Transform[] littleSceneCenters;
 
-    // 盒子
     [SerializeField] private Transform box;
 
-    // 主摄像机
     [SerializeField] private Transform camMain;
-
 
     private float m_deltaX;
     private float m_deltaY;
     [SerializeField] private float m_rotateSpeed;
 
     [SerializeField] private Vector3 lookOffset = new Vector3(0, 1, 0);
-
     [SerializeField] private Vector3 lookFaceOffset = new Vector3(0, -0.5f, 0);
 
     [SerializeField] private float moveYSpeed = 0.05f;
-
     [SerializeField] private float moveZSpeed = 0.1f;
 
-    private int level = 0;
+    private float level = 0f; 
 
     [SerializeField] private Animator ani;
+    [SerializeField] private GameObject RealObject;
 
     private bool canRotate = true;
 
     void Update()
     {
         if (!canRotate || !Input.GetMouseButton(0)) return;
+
         m_deltaX = Input.GetAxis("Mouse X");
         m_deltaY = Input.GetAxis("Mouse Y");
 
-        if (m_deltaX == 0 && m_deltaY == 0)
-        {
-            return;
-        }
-        // 左右旋转盒子
+        if (m_deltaX == 0 && m_deltaY == 0) return;
+
         box.Rotate(Vector3.up, -m_deltaX);
-        // 左右旋转每个小场景
+
         for (int i = 0, len = littleSceneCenters.Length; i < len; ++i)
         {
             littleSceneCenters[i].Rotate(Vector3.up, -m_deltaX * m_rotateSpeed);
         }
 
-
-        // 移动主摄像机
+        // main camera movement
         camMain.localPosition += new Vector3(0, m_deltaY * moveYSpeed, m_deltaY * moveZSpeed);
 
-
-        // 限制主摄像机的移动区域
         if (LimitCamPos()) return;
 
-        // 移动小场景摄像机
         for (int i = 0, len = littleSceneCams.Length; i < len; ++i)
         {
             var cam = littleSceneCams[i];
-            cam.localPosition += new Vector3(0, m_deltaY * moveYSpeed, m_deltaY * moveZSpeed);
+            cam.localPosition += new Vector3(0, m_deltaY * moveYSpeed, 0);
             cam.LookAt(littleSceneCenters[i].position + lookFaceOffset);
         }
 
-        // 检查是否触发了机关
+
         CheckLevelCondition();
     }
 
-    // 限制主摄像机的移动区域 
+
     private bool LimitCamPos()
     {
         var curPos = camMain.position;
         var isOut = false;
-        if (curPos.z < -5f)
-        {
-            curPos.z = -5f; isOut = true;
-        }
 
-        if (curPos.z > 1.2f)
-        {
-            curPos.z = 1.2f; isOut = true;
-        }
-        if (curPos.y > 4.8f)
-        {
-            curPos.y = 4.8f; isOut = true;
-        }
-        if (curPos.y < 1.3f)
-        {
-            curPos.y = 1.3f; isOut = true;
-        }
+        if (curPos.z < -7f) { curPos.z = -7f; isOut = true; }
+        if (curPos.z > 1.2f) { curPos.z = 1.2f; isOut = true; }
+        if (curPos.y > 4.8f) { curPos.y = 4.8f; isOut = true; }
+        if (curPos.y < 1.3f) { curPos.y = 1.3f; isOut = true; }
+
         camMain.position = curPos;
         camMain.LookAt(box.position + lookOffset);
+
         return isOut;
     }
 
     /// <summary>
-    /// 检测是否触发了机关
+    /// detection
     /// </summary>
     private void CheckLevelCondition()
     {
-       if (Vector3.Distance(camMain.position, new Vector3(0, 2.242501f, -3.405001f)) <= 0.5f &&
-            Math.Abs(camMain.localEulerAngles.x - 20.047f) <= 2 &&
-            0 == level)
+        if (Vector3.Distance(camMain.position, new Vector3(0f, 2.272501f, -5.07000f)) <= 0.5f &&
+            Math.Abs(camMain.localEulerAngles.x - 11.22795f) <= 2f &&
+            level == 0f)
         {
             Debug.Log("触发了机关");
             StartCoroutine(NextLevel());
         }
+
+        if (Vector3.Distance(camMain.position, new Vector3(0f, 2.272501f, -5.07000f)) <= 2f)
+        {
+            Debug.Log($"[DEBUG] Cam Pos: {camMain.position} | Cam Rot X: {camMain.localEulerAngles.x} | Level: {level}");
+        }
     }
 
-
-    // 下一关
+    // nextlevel
     private IEnumerator NextLevel()
     {
         canRotate = false;
-        ++level;
-        ani.SetInteger("level", level);
-        yield return null;
+        level = 1f;
+        ani.SetInteger("level", 1); 
+        yield return new WaitForSeconds(2f);
+        RealObject.SetActive(true);
+
         ani.SetInteger("level", 0);
-        // 四秒后才允许旋转
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(4f);
         canRotate = true;
     }
 }
